a201cb07147b658b82ad0c33d60ba52c7969a1f2
St-Nischal <nishalpoudel50@gmail.com>
2025-05-17 19:59:44 +0100

    Completed creating a blockchain

--- src/Block.java
+++ src/Block.java
@@ -6,6 +6,7 @@ public class Block {
     public String previousHash;
     private String data;
     private long timeStamp;
+    private int nonce;
 
     //Block Constructor
     public Block(String data, String previousHash){
@@ -19,9 +20,21 @@ public class Block {
         String calculatehash = StringUtil.applySha256(
                 previousHash +
                         Long.toString(timeStamp)+
+                        Integer.toString(nonce)+
                         data
         );
         return calculatehash;
+    }
+    public void mineBlock(int difficulty){
+
+        // Creates a string with difficulty * "0"
+        String target = new String(new char[difficulty]).replace('\0','0');
+        while(!hash.substring(0,difficulty).equals(target))
+        {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " +hash);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,64 @@
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+    public static ArrayList<Block> blockChain = new ArrayList<>();
+    public static int difficulty = 5;
+
     public static void main(String[] args) {
 
-        Block genesisBlock = new Block("Hi I am the first block","0");
-        System.out.println("Hash for block 1 : " + genesisBlock);
+        blockChain.add(new Block("Hi I am the first block","0"));
+        System.out.println("Trying to Mine block 1... ");
+        blockChain.get(0).mineBlock(difficulty);
 
-        Block secondBlock = new Block("Yo im the second", genesisBlock.hash);
-        System.out.println("Hash for block 2 :" + secondBlock.hash);
+        blockChain.add(new Block("Yo im the second", blockChain.get(blockChain.size()-1).hash));
+        System.out.println("Trying to Mine block 2... ");
+        blockChain.get(1).mineBlock(difficulty);
 
-        Block thirdBlock = new Block("Hey im the third block",secondBlock.hash);
-        System.out.println("Hash for Block 3 : " + thirdBlock.hash);
+        blockChain.add(new Block("Hey im the third block", blockChain.get(blockChain.size()-1).hash));
+        System.out.println("Trying to Mine block 3... ");
+        blockChain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockchain is Valid: " + isChainValid());
+
+        String blockChainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockChain);
+        System.out.println();
+        System.out.println(blockChainJson);
     }
+
+    public static Boolean isChainValid(){
+        Block curretBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0','0');
+
+        //Loop through blockchain to check hash
+        for (int i = 1; i < blockChain.size(); i++) {
+            curretBlock = blockChain.get(i);
+            previousBlock = blockChain.get(i - 1);
+
+            //compare registered hash and calculate hash
+            if (!curretBlock.hash.equals(curretBlock.calculateHash())) {
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            //Compare previous hash and registered previous hash
+            if (!previousBlock.hash.equals(curretBlock.previousHash))
+            {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+
+            //Check if hash is solved
+            if(!curretBlock.hash.substring(0, difficulty).equals(hashTarget)){
+                System.out.println("This block hasnot been mined");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
